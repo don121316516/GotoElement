98c12517263e81c710ed7582c849b1df28b8e883
hhhh8815 <55180599+hhhh8815@users.noreply.github.com>
2020-07-12 14:22:13 +0800

    完成

--- goelment.js
+++ goelment.js
@@ -1,13 +1,29 @@
 window.onload = () => {
-    console.log("頁面載入完成");
-    var test = document.getElementById("test");
 
-    test.onclick = () => {
-        var target = test.getAttribute("data-gte-target");
-        var offset = test.getAttribute("data-gte-offset");
+    // 取得單一元素
+    // var test = document.getElementById("test");
 
-        console.log(target);
-        console.log(offset);
+    // 取得所有元素
+    var elements = document.getElementsByTagName("*");
+    
+    for (let index = 0; index < elements.length; index++) {
+        const e = elements[index];
+        
+        // 元素.點擊事件
+        e.onclick = () => {
+            var target = e.getAttribute("data-gte-target");  // 目標屬性
+            var offset = e.getAttribute("data-gte-offset");  // 位移屬性
+    
+            if (target) {
+
+                var element = document.getElementById(target);      // 目標元素
+        
+                // 視窗.捲動至({ 上方，行為 })
+                window.scrollTo({
+                    top: element.offsetTop - offset,
+                    behavior: "smooth"
+                })
+            }
+        }
     }
 }
-
